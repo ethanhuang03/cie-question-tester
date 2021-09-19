1781c1514961f27673d64bea4b4f056552613d0c
ethanhuang03 <ethanhuang301@gmail.com>
2021-09-19 21:04:56 +1200

    have links for 2009 to 2016 papers

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
-import paper_scraper
+import paper_scraper as ps
 
 if __name__ == '__main__':
-    paper_scraper.paperScraper().print_link()
+    # loop through year
+        # loop through pages
+    year = [2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020, 2021]
+    for year in year:
+        print(year)
+        ig_maths = ps.pastPaper(category=3, subject=11, year=year)
+        ig_maths.scrape_paper()
+
--- paper_scraper.py
+++ paper_scraper.py
@@ -1,9 +1,13 @@
-class paperScraper(object):
-    def __init__(self, category="", subject="", year="", season="", paper="", time_zone="", chapter="", website_page=""):
-        self.category = category  # 3: IGCSE, 5:A Level, 7:IB Diploma
+import urllib.request, urllib.error, urllib.parse
+
+
+class pastPaper(object):
+    def __init__(self, category="", subject="", year="", season="", paper="", time_zone="", chapter="",
+                 website_page=""):
+        self.category = category  # 3: IGCSE, 5: A Level, 7: IB Diploma
         self.subject = subject  # still need to be decoded
         self.year = year  # past paper year
-        self.season = season  # or "winter" or "summer,winter"
+        self.season = season  # "winter" or "summer,winter"
         self.paper = paper  # dependant on subject, input can also be "1", "1,2" etc.
         self.time_zone = time_zone  # 0 is IB only, input can also be "1", "1,2,3,4,5" etc.
         self.chapter = chapter  # still need to be decoded
@@ -14,3 +18,27 @@ class paperScraper(object):
 
     def print_link(self):
         print(self.link)
+
+    def find_offset(self):
+        response = urllib.request.urlopen(self.link)
+        webpage = response.read().decode('utf-8')
+        for line in webpage.split('\n'):
+            if "offset=" in line:
+                print(line)
+
+    def scrape_paper(self):
+        response = urllib.request.urlopen(self.link)
+        webpage = response.read().decode('utf-8')
+        for line in webpage.split('\n'):
+            if "/questions" in line:
+                parsed = line[line.find("/questions"):]
+                parsed = parsed.replace("""');">Question</a>""", "")
+                parsed = parsed.replace("""');">Answer</a>""", "")
+                parsed = parsed.replace("', '", " ")
+                print(parsed.split()[0] + " " + ' '.join(parsed.split()[2:]))
+
+        '''
+        f = open('temp.html', 'w')
+        f.write(webpage)
+        f.close()
+        '''
